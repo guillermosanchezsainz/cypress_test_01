1985c2095671e4f94559d4c42b8c37ffb9944363
gsanchez@protecmedia.com <gsanchez@protecmedia.com>
2023-06-30 12:21:44 +0200

    actualizacion 001

--- cypress/e2e/hola_mundo.cy.js
+++ cypress/e2e/hola_mundo.cy.js
@@ -2,24 +2,24 @@ require('cypress-xpath');
 
 function TestLogin()
 {
-  /*
+  
   cy.get("input[name='nameUser']").clear();
   cy.get("input[name='namePwd']").clear();
   cy.wait(1300);
   cy.get("input[name='nameUser']").type("guillermo");
   cy.get("input[name='namePwd']").type("a");
   cy.get("button.btn-submit").click();
- */
-  cy.get('[shipoid="_nameUser"]').clear();
+ 
+/*   cy.get('[shipoid="_nameUser"]').clear();
   cy.get('[shipoid="_namePwd"]').clear();
-  cy.wait(1300);
+  // cy.wait(1300);
   cy.get('[shipoid="_nameUser"]').type("guillermo");
   cy.get('[shipoid="_namePwd"]').type("a")
   cy.get('[shipoid="_btnLogin"]').click();
-
+ */
   
 
-  cy.wait(4300);
+  // cy.wait(4300);
 }
 
 function TestErrorLogin()
@@ -27,37 +27,37 @@ function TestErrorLogin()
   cy.get("input[name='nameUser']").type("guillermo");
   cy.get("input[name='namePwd']").type("guillermo");
   cy.get("button.btn-submit").click();
-  cy.wait(1300);
+  //cy.wait(1300);
 }
 
 function AbrirMenuSeccion()
 {
   cy.xpath("/html/body/app-root/conf-page/div/mat-toolbar/mat-toolbar-row/shared-module-navigator/button").click();
-  cy.wait(100);
+  //cy.wait(100);
 }
 
 function SeleccionarSuscripciones()
 {
   cy.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/shared-module-card/mat-card/mat-card-content[1]/mat-card-content[1]/img").click();
-  cy.wait(100);
+  //cy.wait(100);
 }
 
 function AbrirMenuSuscripciones()
 {
   cy.xpath("/html/body/app-root/susc-page/div/mat-toolbar/mat-toolbar-row/button").click();
-  cy.wait(100);
+  //cy.wait(100);
 }
 
 function AbrirGestionComercial()
 {
   cy.xpath("/html/body/div[2]/div[2]/div/div/div/button[1]").click();
-  cy.wait(100);
+  //cy.wait(100);
 }
 
 function AbrirCrmSuscripcion()
 {
   cy.xpath("/html/body/div[2]/div[3]/div/div/div/button[1]").click();
-  cy.wait(2100);
+  //cy.wait(2100);
 }
 
 function EscribirBuscadosCrmSuscripciones(str)
@@ -70,39 +70,39 @@ function EscribirBuscadosCrmSuscripciones(str)
 function AbrirMenuSeccionDesdeSuscripciones()
 {
   cy.xpath("/html/body/app-root/susc-page/div/mat-toolbar/mat-toolbar-row/shared-module-navigator/button").click();
-  cy.wait(100);
+  //cy.wait(100);
 }
 
 function SeleccionarConfiguracionDesdeSuscripciones()
 {
   cy.get("img.fontA-icon-module").should('be.visible').click();
-  cy.wait(300);
+  //cy.wait(300);
 }
 
 function AbrirMenuUsuarioDesdeConfiguracion()
 {
   cy.xpath("/html/body/app-root/conf-page/div/mat-toolbar/mat-toolbar-row/shared-button-user-card/button").click();
-  cy.wait(100);
+  //cy.wait(100);
 }
 
 function FinalizarDesdeConfiguracion()
 {
   cy.xpath("/html/body/div[2]/div[7]/div/mat-dialog-container/shared-user-card/mat-card/mat-card-content/mat-card-content[3]/button").click();
-  cy.wait(100);
+  //cy.wait(100);
 }
 
 function ConfirmarFinalizar()
 {
   cy.xpath("/html/body/div[2]/div[7]/div/mat-dialog-container/dlg-shared-info/div[2]/button[2]").click();
-  cy.wait(1100);
+  //cy.wait(1100);
 }
 
 describe('pppp', () =>{
   it('xxxx', () =>{
     cy.log('popopo');
-    // cy.visit('http://mdesashipo.protecmedia.com/shipov25desa/');
-    cy.visit('http://localhost:4200');
-    cy.wait(1300);
+    cy.visit('http://mdesashipo.protecmedia.com/shipov25desa/');
+    // cy.visit('http://localhost:4200');
+    //cy.wait(1300);
     
     TestErrorLogin();    
     TestLogin();
